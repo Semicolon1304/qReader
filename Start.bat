d1d9ea8481c59a73f62eba2740dfb96ec1d52b43
Zack McKenzie <51973976+Semicolon1304@users.noreply.github.com>
2020-08-17 10:24:22 -0500

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d C:\Users\example\OneDrive\Desktop\qRecog\venv\Scripts & activate.bat & cd /d   C:\Users\example\OneDrive\Desktop\qRecog & python qRecog.py"
+cmd /k "cd /d C:\Users\example\OneDrive\Desktop\qReader\venv\Scripts & activate.bat & cd /d   C:\Users\example\OneDrive\Desktop\qReader & python qReader.py"
